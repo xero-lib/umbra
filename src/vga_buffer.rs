03f47bfe1242c5baac379aa69311bebd24a046fa
xero-lib <jonathankurtz@protonmail.com>
2023-05-28 18:49:44 -0500

    project: impl vga_buff access

---
+++ src/vga_buffer.rs
@@ -0,0 +1,113 @@
+#[allow(dead_code)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u8)]
+pub enum Color {
+    Black = 0,
+    Blue,
+    Green,
+    Cyan,
+    Red,
+    Magenta,
+    Brown,
+    LightGray,
+    DarkGray,
+    LightBlue,
+    LightGreen,
+    LightCyan,
+    LightRed,
+    Pink,
+    Yellow,
+    White
+}
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(transparent)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new(foreground: Color, background: Color) -> Self {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(C)]
+struct ScreenChar {
+    ascii_character: u8,
+    color_code: ColorCode
+}
+
+const BUFFER_HEIGHT: usize = 25;
+const BUFFER_WIDTH: usize = 80;
+
+#[repr(transparent)]
+struct Buffer {
+    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_HEIGHT],
+}
+
+pub struct Writer {
+    column_position: usize,
+    color_code: ColorCode,
+    buffer: &'static mut Buffer
+}
+
+impl Writer {
+    pub fn write_byte(&mut self, byte: u8) {
+        match byte {
+            b'\n' => self.new_line(),
+            byte => {
+                if self.column_position >= BUFFER_WIDTH { self.new_line() }
+                let row = BUFFER_HEIGHT - 1;
+                let col = self.column_position;
+                let color_code = self.color_code;
+                self.buffer.chars[row][col] = ScreenChar {
+                    ascii_character: byte,
+                    color_code,
+                };
+                self.column_position += 1;
+            }
+        }
+    }
+
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                // printable ASCII byte or newline
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                // not part of printable ASCII range, prints binary repr of given data
+                _ => {
+                    self.write_byte(0xfe);
+                    self.write_string("{0x");
+
+                    let mut rbyte = byte.reverse_bits();
+                    for _ in 0..2 {
+                        let mut num = 0u8;
+                        for _ in 0..4 {
+                            num <<= 1;
+                            num += rbyte & 1;
+                            rbyte >>= 1;
+                        }
+                        
+                        self.write_byte(if num < 10 { num + b'0' } else { num - 10 + b'A' });
+                    }
+                    self.write_byte(b'}');
+                }
+            }
+        }
+    }
+
+    fn new_line(&mut self) {
+        // self.column_position = 0;
+        todo!();
+    }
+}
+
+pub fn print_test() {
+    let mut writer = Writer {
+        column_position: 0,
+        color_code: ColorCode::new(Color::Red, Color::Black),
+        buffer: unsafe { &mut *(0xb8000 as *mut Buffer)}
+    };
+
+    writer.write_byte(b'H');
+    writer.write_string("ellö, Umbra!");
+}
