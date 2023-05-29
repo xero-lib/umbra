2384309a813d42f015d12a2289f755095d4524f4
xero-lib <jonathankurtz@protonmail.com>
2023-05-28 19:32:06 -0500

    vga_buffer: impl fmt for Writer

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,6 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-static HELLO: &[u8] = b"Hello, Umbra!";
-
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     vga_buffer::print_test();
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,4 +1,5 @@
 use volatile::Volatile;
+use core::fmt::{Write, Result};
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -103,6 +104,13 @@ impl Writer {
     }
 }
 
+impl Write for Writer {
+    fn write_str(&mut self, s: &str) -> Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
+
 pub fn print_test() {
     let mut writer = Writer {
         column_position: 0,
@@ -111,5 +119,6 @@ pub fn print_test() {
     };
 
     writer.write_byte(b'H');
-    writer.write_string("ellö, Umbra!");
+    writer.write_string("ello");
+    write!(writer, ", Umbra!").unwrap();
 }
