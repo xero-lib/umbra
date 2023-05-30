6b6ca5d7f167f666bd0289f3e9337a0f4bf77abc
xero-lib <jonathankurtz@protonmail.com>
2023-05-29 20:13:10 -0500

    vga_buffer: added println tests

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -168,3 +168,25 @@ macro_rules! println {
 pub fn _print(args: Arguments) {
     WRITER.lock().write_fmt(args).unwrap();
 }
+
+#[test_case]
+fn test_println_single() {
+    println!("test_println_single output");
+}
+
+#[test_case]
+fn test_println_many() {
+    for _ in 0..200 {
+        println!("test_println_many output");
+    }
+}
+
+#[test_case]
+fn test_println_output() {
+    let s = "Umbral println macro output";
+    println!("{s}");
+    for (i, c) in s.chars().enumerate() {
+        let screen_char = WRITER.lock().buffer.chars[BUFFER_HEIGHT - 2][i].read();
+        assert_eq!(char::from(screen_char.ascii_character), c);
+    }
+}
